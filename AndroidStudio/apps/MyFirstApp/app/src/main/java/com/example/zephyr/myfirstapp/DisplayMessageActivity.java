c2367d068cc7671fc3e3efd802076970223e5915
MicheledG <michele.digirolamo.to@gmail.com>
2015-05-04 23:54:56 +0200

    starting action bar

--- AndroidStudio/apps/MyFirstApp/app/src/main/java/com/example/zephyr/myfirstapp/DisplayMessageActivity.java
+++ AndroidStudio/apps/MyFirstApp/app/src/main/java/com/example/zephyr/myfirstapp/DisplayMessageActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.TextView;
 
@@ -29,16 +30,30 @@ public class DisplayMessageActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+        // Handle presses on the action bar items
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(id) {
+            case R.id.action_search:
+                openSettings();
+                return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+
+
+
+    //ADD OPTIONS TO ACTION BAR
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        //inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_display_message, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
